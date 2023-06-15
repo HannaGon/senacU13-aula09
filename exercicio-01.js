aa7dfec3d0ec205384fbb22ee087813e33708353
ppedro <pp220466@gmail.com>
2023-06-14 21:37:34 -0300

    exercicios resolvidos aula09

--- exercicio-01.js
+++ exercicio-01.js
@@ -1,1 +1,7 @@
 /*acesse o json e retorne título e descrição de todos os filmes*/
+
+function bancoDeDados(){
+    return require("./data/ghibli.json")
+}
+
+console.log(jason())
--- exercicio-02.js
+++ exercicio-02.js
@@ -1,3 +1,18 @@
 /*crie uma função que retorne titulo e
 descrição de um valor no banco com base
 em seu id*/
+
+function bancoDeDados(){
+    return require("./data/ghibli.json")
+}
+
+let id = 7
+function filme(id){
+    for(let variavel of bancoDeDados()){
+    if(variavel.id==id){
+    console.log(`${bancoDeDados()[id].title}\n\n${bancoDeDados()[id].description}`)
+    }
+    }
+}
+
+filme(id)
