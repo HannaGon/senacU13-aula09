17d9ed9a2c993429c7aa30681a9570c2d10127e3
eu <juliohc_101@hotmail.com>
2023-06-14 21:06:25 -0300

    Exercicio-14-06

--- exercicio-02.js
+++ exercicio-02.js
@@ -1,3 +1,19 @@
 /*crie uma função que retorne titulo e
 descrição de um valor no banco com base
 em seu id*/
+
+function bancoDeDados() {
+    return require("./data/ghibli.json")}
+
+let dbghibli = bancoDeDados()
+
+function searchId(id){
+    for (let variavel of dbghibli){
+       if (variavel.id==id) {
+           console.log(variavel.title)
+           console.log(variavel.description)
+       }
+       else{}
+    }
+   }
+   searchId(1)
